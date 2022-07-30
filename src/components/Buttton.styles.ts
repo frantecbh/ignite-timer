bdb557cfb21737bef06000f6bea586d9d1858898
frantecbh <franciscomenezesbh@gmail.com>
2022-07-30 12:42:41 -0300

    styled components

--- src/components/Buttton.styles.ts
+++ src/components/Buttton.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
@@ -19,7 +19,7 @@ width: 100px;
 height: 40px;
 
 ${props => {
-        return `background-color: ${buttonVariants[props.variant]}`
+        return css`background-color: ${buttonVariants[props.variant]}`
     }}
 
 `
