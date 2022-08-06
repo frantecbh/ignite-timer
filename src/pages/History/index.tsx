6fe94f6ab2a651ebeca11b7accf6715fd17fbfa8
frantecbh <franciscomenezesbh@gmail.com>
2022-08-06 12:08:36 -0300

    pagina history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,57 @@
+import { HistoryContainer, HistoryList } from "./styles"
+
+
 export const History = () => {
-  return <h1>History</h1>
+    return (
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa A</td>
+                            <td>20 Minutos</td>
+                            <td>Há 2 Meses</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa A</td>
+                            <td>20 Minutos</td>
+                            <td>Há 2 Meses</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa A</td>
+                            <td>20 Minutos</td>
+                            <td>Há 2 Meses</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa A</td>
+                            <td>20 Minutos</td>
+                            <td>Há 2 Meses</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa A</td>
+                            <td>20 Minutos</td>
+                            <td>Há 2 Meses</td>
+                            <td>Concluído</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </HistoryList>
+
+        </HistoryContainer>
+    )
 }
